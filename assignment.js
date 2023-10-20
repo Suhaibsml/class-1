06d2058f5fd8da40b6e8b60877fecc2b73bd5e99
munazza <137645834+Suhaibsml@users.noreply.github.com>
2023-10-21 00:20:19 +0500

    Initial commit

---
+++ assignment.js
@@ -0,0 +1,148 @@
+// Chapter no # 1
+
+
+// Question # 1
+// var visitors = "Hello !" +" " +"Welcome to our website";
+// alert(visitors); 
+
+
+// Question # 2
+// var error = "Error!" + " " + "Please enter a valid password";
+// alert(error);
+
+
+// Question # 3
+// var welcome = "Welcome to JS Land... <\br>Happy Coding!";
+// alert(welcome);
+
+
+// Question # 4 (a)
+// var message = "Welcome to JS Land...";
+// alert(message);
+
+
+// Question # 4 (b)
+// var message = "Happy Coding!";
+// alert(message);
+
+
+// Question # 5
+// var message = "Hello... I can run JS through my web browser's console";
+// console.log(message);
+
+
+
+// Chapter no # 2
+
+
+// Question # 1
+// var username ;
+
+
+
+// Question # 2
+// var myName = "Suhaib Ismail";
+
+
+// Question # 3
+// var JS = "Hello World";
+// alert(JS);
+
+
+// Question # 4
+// var name = "Johne Doe";
+// var age = "15 years old";
+// var certification = "Certified Mobile Application Development";
+
+// alert(name);
+// alert(age);
+// alert(certification);
+
+
+
+// Question # 5
+// var message = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+// alert(message);
+
+
+// Question # 6
+// var email = "example@example.com";
+// alert("My email address is" + " " + email);
+
+
+// Question # 7
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the Book" + " " + book);
+
+
+// Question # 8
+// document.write("Yah! I can write HTML content through JavaScript");
+
+
+// Question # 9
+// var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(design);
+
+
+
+// Chapter no # 3
+
+
+// Question # 1
+// var age = 22;
+// alert("I am " + age + " years old");
+
+
+// Question # 2
+// var website = view;
+// var view = 11;
+// alert(" You have visited this site " + view + " times");
+
+
+// Question # 3
+// var birthYear = 2001;
+// document.write("My birth year is " + birthYear);
+
+
+// Question # 4
+// var Visitors_name = "<b>John Doe</b>";
+// var product_title = "<b>XYZ Clothing store</b>";
+// var Quantity = "<b>5 T-shirts(s)</b>";
+
+// document.write(Visitors_name + " " + "ordered " + Quantity + " " + "on " + product_title);
+
+
+
+
+// Chapter no # 4
+
+
+// Question # 1
+// var num1 = 30 , num2 = 45 , num3 = 27 ;
+
+
+// Question # 2
+
+// legal variables:
+
+// 1. myName
+// 2. my_num
+// 3. _value
+// 4. $num3
+// 5. userAge
+
+// illegal variables:
+
+// 1. 123Name
+// 2. @mynum
+// 3. my-name
+// 4. if
+// 5. function
+    
+
+
+// Question # 3
+
+// var heading = "<h2>Rules for naming JS variables</h2><div>Variable names can only contain , numbers , $ and ______. For example : $my_1stVariable</div> <div>Variables must begin with a letter, $ or _____. For example $name, _name or name</div> <div>Variable names are case keywords</div>";
+
+// document.write(heading);
